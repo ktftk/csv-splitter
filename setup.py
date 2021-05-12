d972bc919658eb392b28058e1cff7aae3e198e9e
ktftk <62935224+ktftk@users.noreply.github.com>
2021-05-12 13:16:34 +0900

    chore: add setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ with open("README.md") as f:
     readme = f.read()
 
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
 setup(
     name="csv_splitter",
     version="0.0.1",
     description="",
     long_description=readme,
     author="ktftk",
-    url="",
+    url="https://github.com/ktftk/csv_splitter",
     packages=find_packages(),
-    install_requires=_requires_from_file("requirements.txt"),
-    include_package_data=True,
 )
